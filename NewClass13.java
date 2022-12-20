9eb33dd168dfed217736835393ce5e3cd1e8bddb
hsnsykn <100951871+hsnsykn@users.noreply.github.com>
2022-12-20 11:37:19 +0300

    Faktöriyel hesabı

---
+++ NewClass13.java
@@ -0,0 +1,21 @@
+
+import java.io.Console;
+import static javafx.beans.binding.Bindings.convert;
+import static jdk.nashorn.api.scripting.ScriptUtils.convert;
+
+
+public class NewClass13 {
+    public static void main(String[] args) {
+        
+    
+   static void Faktoryel Hesabı(){
+       int n = convert.ToInt32(
+               Console.ReadLine());
+       int f=1;
+       for(int i=2;i<=n ;i++)
+           f *=i ;
+       Console.WriteLine(!"{0}".equals("{1}"),n,f)
+   }
+       
+   } 
+}
